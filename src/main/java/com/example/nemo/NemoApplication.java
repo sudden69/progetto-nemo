7ddff0bd749571a1c92f37203adf58966f575d20
rot13-git <mirielloraffaele@gmail.com>
2020-06-20 19:07:34 +0200

    Davide ciao era una prova

--- src/main/java/com/example/nemo/NemoApplication.java
+++ src/main/java/com/example/nemo/NemoApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NemoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(NemoApplication.class, args);
     }
